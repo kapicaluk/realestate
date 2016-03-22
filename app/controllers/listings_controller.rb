939fbfcb9f30df4ea19d8a975f325184fd6e88cd
Lukasz Kapica <kapicaluk@Lukaszs-MacBook-Pro.local>
2016-03-21 20:58:10 -0400

    one more change from the class

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,4 +2,24 @@ class ListingsController < ApplicationController
   def index	
   	@listings = Listing.all
   end
+
+
+def new
+	@listings - Listing.new
+
+end
+
+def create
+end
+
+def edit
+end
+
+def destroy
+end
+
+def show
+end
+
+
 end
